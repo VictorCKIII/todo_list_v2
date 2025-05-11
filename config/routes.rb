62c69ba1e4a51cc19d673fbb319b5b0858ba3909
Victor <eduardovictor822@gmail.com>
2025-05-10 23:56:21 -0300

    Autenticação Adicionada

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   resources :lists do
     resources :items, only: [ :create, :edit, :update, :destroy ]
   end
+  resource :session, only: [:new, :create, :destroy]
   get "up" => "rails/health#show", as: :rails_health_check
 
 
 
-  root "lists#index"
+  root "sessions#new"
+  
 end
