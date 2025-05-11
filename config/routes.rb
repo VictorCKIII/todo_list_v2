9613e53b59b78e920058dc5c2bdb0ecfc1065b1e
Victor <eduardovictor822@gmail.com>
2025-05-11 00:02:48 -0300

    Issues Fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   resources :lists do
     resources :items, only: [ :create, :edit, :update, :destroy ]
   end
-  resource :session, only: [:new, :create, :destroy]
+  resource :session, only: [ :new, :create, :destroy ]
   get "up" => "rails/health#show", as: :rails_health_check
 
 
 
   root "sessions#new"
-  
 end
